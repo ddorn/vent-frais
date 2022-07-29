74be0eff4847a0bc91f28a9206fca68df3b0c97f
ddorn <diego.dorn@free.fr>
2022-07-29 03:40:13 +0200

    format

--- card_draw.py
+++ card_draw.py
@@ -16,7 +16,6 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import pygame
 
-
 from constants import *
 
 
@@ -39,41 +38,39 @@ def gen_points(density):
         ]) for x in range(int(W // GRID_SIZE))
     ]
 
-    col_weight = cum_sum([line_weights[x][-1] for x in range(int(W // GRID_SIZE))])
+    col_weight = cum_sum(
+        [line_weights[x][-1] for x in range(int(W // GRID_SIZE))])
     deja_vu = set()
 
     while True:
         r = uniform(0, col_weight[-1])
         x = bisect_left(col_weight, r)
         y = bisect_left(line_weights[x], uniform(0, line_weights[x][-1]))
-        if not((x,y) in deja_vu):
+        if not ((x, y) in deja_vu):
             yield (np.array([x, y]) * GRID_SIZE)
-            deja_vu.add((x,y))
+            deja_vu.add((x, y))
 
 
 def get_centroid(cell):
     return np.array(Polygon(cell).centroid.coords)[0]
 
 
-
 def get_relaxed_points():
 
-    def density(x,y):
+    def density(x, y):
         return 1.
 
     points = []
     N = 1000
-    MAX_ITERATION = 5 #10
-    rho = 0.5 #0
+    MAX_ITERATION = 5  #10
+    rho = 0.5  #0
     SAMPLING_MARGIN = 0.2
     MAX_MOUV = 0.002
 
-
     pts_gen = gen_points(density=density)
 
-
     for i in range(N):
-        p = np.random.uniform(-SAMPLING_MARGIN, 1+SAMPLING_MARGIN, 2)
+        p = np.random.uniform(-SAMPLING_MARGIN, 1 + SAMPLING_MARGIN, 2)
 
         if 0 < p[0] and p[0] < 1 and 0 < p[1] and p[1] < 1:
             points.append(next(pts_gen))
@@ -82,10 +79,9 @@ def get_relaxed_points():
 
     points = np.array(points)
 
-
     for it in range(MAX_ITERATION):
 
-        vor = Voronoi(points, qhull_options = "Qbb Qz Qc")
+        vor = Voronoi(points, qhull_options="Qbb Qz Qc")
 
         centroids = []
 
@@ -100,15 +96,13 @@ def get_relaxed_points():
 
         ## move toward centroids
         for k in range(N):
-            vect = centroids[k]-points[k]
+            vect = centroids[k] - points[k]
             if np.linalg.norm(vect) > MAX_MOUV:
-                points[k] = points[k] + MAX_MOUV*vect/np.linalg.norm(vect)
+                points[k] = points[k] + MAX_MOUV * vect / np.linalg.norm(vect)
             else:
-                points[k] = (1-rho)*points[k] + rho*centroids[k]
+                points[k] = (1 - rho) * points[k] + rho * centroids[k]
 
-
-    vor = Voronoi(points, qhull_options = "Qbb Qz Qc")
-
+    vor = Voronoi(points, qhull_options="Qbb Qz Qc")
 
     inner_circle_radii = []
     for k in range(N):
@@ -116,25 +110,27 @@ def get_relaxed_points():
         reg_corners = vor.regions[vor.point_region[k]]
         rad = 100
         for corner in range(len(reg_corners)):
-            p1 = vor.vertices[reg_corners[corner-1]]
+            p1 = vor.vertices[reg_corners[corner - 1]]
             p2 = vor.vertices[reg_corners[corner]]
             #print(p1, p2, p3)
-            rad = min(rad, np.linalg.norm(np.cross(p2-p1, p1-p3))/np.linalg.norm(p2-p1))
+            rad = min(
+                rad,
+                np.linalg.norm(np.cross(p2 - p1, p1 - p3)) /
+                np.linalg.norm(p2 - p1))
         inner_circle_radii.append(rad)
-
 
     return centroids, inner_circle_radii, vor
 
 
-
 def getLine(cx, cy, angle, length, sf):
-    l = length/2
-    x1 = cx + np.cos(angle)*l
-    y1 = cy + np.sin(angle)*l
+    l = length / 2
+    x1 = cx + np.cos(angle) * l
+    y1 = cy + np.sin(angle) * l
 
-    x2 = cx - np.cos(angle)*l
-    y2 = cy - np.sin(angle)*l
+    x2 = cx - np.cos(angle) * l
+    y2 = cy - np.sin(angle) * l
     return x1, y1, x2, y2
+
 
 @dataclass
 class ProtectedZones:
@@ -154,11 +150,13 @@ class ProtectedZones:
             if self.is_face:
                 for rect in self.line_rects:
                     inflation = self.RECT_INFLATION + r * 500
-                    if rect.inflate(inflation, inflation).collidepoint(self.to_pygame(p)):
+                    if rect.inflate(inflation,
+                                    inflation).collidepoint(self.to_pygame(p)):
                         return True
                 return False
             else:
-                return np.linalg.norm(self.logo_xy - p) <= (self.logo_radius + r)
+                return np.linalg.norm(self.logo_xy - p) <= (self.logo_radius +
+                                                            r)
 
         else:  # shape_type == "line":
             p1 = np.array([shape_dict["x1"], shape_dict["y1"]])
@@ -166,7 +164,8 @@ class ProtectedZones:
             if self.is_face:
                 inflation = self.RECT_INFLATION + self.line_width
                 for rect in self.line_rects:
-                    if rect.inflate(inflation, inflation).clipline(self.to_pygame(p1), self.to_pygame(p2)):
+                    if rect.inflate(inflation, inflation).clipline(
+                            self.to_pygame(p1), self.to_pygame(p2)):
                         return True
                 return False
             else:
@@ -178,15 +177,17 @@ class ProtectedZones:
 
 
 Field2D = Callable[[float, float], float]
+
+
 def draw_card(
     card_type: Category,
     text_metrics: list[tuple[str, tuple[int, int], pygame.Rect]],
     is_face: bool = False,
     angles: Field2D = lambda x, y: 0,
     intensity: Field2D = lambda x, y: 0,
-    ):
+):
 
-        # TODO seed the card
+    # TODO seed the card
     S = 500
     shrink_factor = 0.85
 
@@ -198,20 +199,40 @@ def draw_card(
         line_width=5,
     )
 
+    d = draw.Drawing(S, S, origin=(0, 0), displayInline=False)
 
-    d = draw.Drawing(S, S, origin=(0,0), displayInline=False)
-
-
-
-    d.append(draw.Rectangle(0,0,S,S, fill=COLOR_PALETTES[card_type]['background'], rx=40, ry=40))
+    d.append(
+        draw.Rectangle(0,
+                       0,
+                       S,
+                       S,
+                       fill=COLOR_PALETTES[card_type]['background'],
+                       rx=40,
+                       ry=40))
 
     bg = draw.ClipPath()
-    bg.append(draw.Rectangle(0,0,S,S, fill=COLOR_PALETTES[card_type]['background'], rx=40, ry=40))
+    bg.append(
+        draw.Rectangle(0,
+                       0,
+                       S,
+                       S,
+                       fill=COLOR_PALETTES[card_type]['background'],
+                       rx=40,
+                       ry=40))
 
     if is_face:
         # font size 1000 = 1 en coo
         for line, (x, y), rect in text_metrics:
-            d.append(draw.Text([line], 30, x, 500 - y, fill='white', style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Arbutus Slab';-inkscape-font-specification:'Arbutus Slab'"))
+            d.append(
+                draw.Text(
+                    [line],
+                    30,
+                    x,
+                    500 - y,
+                    fill='white',
+                    style=
+                    "font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Arbutus Slab';-inkscape-font-specification:'Arbutus Slab'"
+                ))
 
     else:
         #d.append(draw.Circle(NoMansLand.LOGO_XY[0]*S, NoMansLand.LOGO_XY[1]*S, NoMansLand.LOGO_RADIUS*S,
@@ -232,42 +253,73 @@ def draw_card(
         #d.append(draw.Rectangle(NoMansLand.LOGO_XY[0]*S-LOGO_W/2,
         #                    NoMansLand.LOGO_XY[1]*S-LOGO_H/2,LOGO_W,LOGO_H, fill="white", style="opacity:0.5"))
 
-
-
     pts, radii, vor = get_relaxed_points()
 
-
-    for i,p in enumerate(pts):
+    for i, p in enumerate(pts):
         if SHOW_DESIGN:
             if np.random.random() < PROP_CIRCLE:
                 shape_color = rd.choice(COLOR_PALETTES[card_type]['dots'])
-                if not NoMansLand.collide({"cx":p[0], "cy":p[1], "r":radii[i]}, "circle"):
-                    d.append(draw.Circle(p[0]*S, p[1]*S, radii[i]*S*shrink_factor,
-                        fill=shape_color, stroke='none', clip_path=bg))
+                if not NoMansLand.collide(
+                    {
+                        "cx": p[0],
+                        "cy": p[1],
+                        "r": radii[i]
+                    }, "circle"):
+                    d.append(
+                        draw.Circle(p[0] * S,
+                                    p[1] * S,
+                                    radii[i] * S * shrink_factor,
+                                    fill=shape_color,
+                                    stroke='none',
+                                    clip_path=bg))
 
             else:
                 shape_color = rd.choice(COLOR_PALETTES[card_type]['lines'])
                 angle = np.random.uniform(0, 6.28)
-                x1,y1,x2, y2 = getLine(p[0], p[1], angle, radii[i], shrink_factor)
-                if not NoMansLand.collide({"x1": x1, "y1": y1, "x2": x2, "y2": y2}, "line"):
-                    d.append(draw.Line(x1*S, y1*S, x2*S, y2*S,
-                        fill='none', stroke_width=LINE_WIDTH,
-                        stroke=shape_color,stroke_linecap= "round", clip_path=bg))
+                x1, y1, x2, y2 = getLine(p[0], p[1], angle, radii[i],
+                                         shrink_factor)
+                if not NoMansLand.collide(
+                    {
+                        "x1": x1,
+                        "y1": y1,
+                        "x2": x2,
+                        "y2": y2
+                    }, "line"):
+                    d.append(
+                        draw.Line(x1 * S,
+                                  y1 * S,
+                                  x2 * S,
+                                  y2 * S,
+                                  fill='none',
+                                  stroke_width=LINE_WIDTH,
+                                  stroke=shape_color,
+                                  stroke_linecap="round",
+                                  clip_path=bg))
 
         if SHOW_VOR:
             #print(p, radii)
 
-            d.append(draw.Circle(p[0]*S, p[1]*S, radii[i]*S,
-                fill='none', stroke_width=0.5, stroke='blue'))
-            d.append(draw.Circle(p[0]*S, p[1]*S, 1.,
-                    fill='black'))
+            d.append(
+                draw.Circle(p[0] * S,
+                            p[1] * S,
+                            radii[i] * S,
+                            fill='none',
+                            stroke_width=0.5,
+                            stroke='blue'))
+            d.append(draw.Circle(p[0] * S, p[1] * S, 1., fill='black'))
             reg_corners = vor.regions[vor.point_region[i]]
             for corner in range(len(reg_corners)):
-                if reg_corners[corner-1] != -1 and reg_corners[corner] != -1:
-                    p1 = vor.vertices[reg_corners[corner-1]]
+                if reg_corners[corner - 1] != -1 and reg_corners[corner] != -1:
+                    p1 = vor.vertices[reg_corners[corner - 1]]
                     p2 = vor.vertices[reg_corners[corner]]
-                    d.append(draw.Line(p1[0]*S, p1[1]*S, p2[0]*S, p2[1]*S,
-                        stroke='red', stroke_width=0.5, fill='none'))
+                    d.append(
+                        draw.Line(p1[0] * S,
+                                  p1[1] * S,
+                                  p2[0] * S,
+                                  p2[1] * S,
+                                  stroke='red',
+                                  stroke_width=0.5,
+                                  fill='none'))
     d.setPixelScale(1.5)  # Set number of pixels per geometry unit
 
     return d.asSvg()
